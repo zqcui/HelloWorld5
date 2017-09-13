0eaf12a9ab1393b02da026c0becfe92cd848add7
czq <czq@bistu.edu.cn>
2017-09-13 08:17:58 +0800

    add comments

--- app/src/main/java/se/cs/bistu/edu/cn/helloworld5/MainActivity.java
+++ app/src/main/java/se/cs/bistu/edu/cn/helloworld5/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //add comments
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
